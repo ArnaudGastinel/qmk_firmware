fe5b6453d9c3ac127c64debfc74b2065b9395645
Arnaud Gastinel <arnaud.gastinel@avisto.com>
2022-11-22 09:34:05 +0100

    First implementation for PaF's planck

---
+++ keyboards/planck/keymaps/paf/rules.mk
@@ -0,0 +1,18 @@
+SRC += muse.c
+SRC += features/caps_word.c
+
+COMMAND_ENABLE   = no   # Disable shift combination, which conflicts with shift-parens
+NKRO_ENABLE      = yes  # N-key rollover required for use as a steno board
+AUDIO_ENABLE     = yes   # Audio output on port C6
+TAP_DANCE_ENABLE = yes
+MOUSEKEY_ENABLE  = yes
+CONSOLE_ENABLE = no
+BACKLIGHT_ENABLE = yes
+LTO_ENABLE = yes
+GRAVE_ESC_ENABLE = no 
+MAGIC_ENABLE = no
+EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)`
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+COMBO_ENABLE = yes
+CONSOLE_ENABLE = yes
+UNICODE_ENABLE = no
